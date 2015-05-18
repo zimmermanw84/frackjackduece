c7b2c1d54c63a93e286c97ad2062fc744ca1f45b
Walter Zimmerman <zimmermanw84@gmail.com>
2015-05-18 11:55:48 -0700

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	entry: ['./app/main.js'],
+	output: {
+		path: './build',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{ test: /\.js$/, loader: 'jsx-loader' }
+		]
+	}
+};
+
