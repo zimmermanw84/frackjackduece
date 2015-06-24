934e296a91eac73c6969f97ecfdf0808f3ea90fc
Walter Zimmerman <zimmermanw84@gmail.com>
2015-06-24 07:17:59 -0700

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,13 @@ var config = {
     this.resolve.alias[name] = path;
     this.module.noParse.push(new RegExp('^' + name + '$'));
   },
-  
+
   entry: {
     app: ['./app/main.jsx'],
     vendors: ['react', 'bootstrap', 'jquery']
   },
   resolve: { alias: {} },
-  
+
   plugins: [
     new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
   ],
@@ -25,12 +25,11 @@ var config = {
     noParse: [],
     loaders: [
       { test: /\.jsx$/, loader: 'jsx-loader' },
-//			{ test: /\.js$/, loader: 'script-loader' },
-			{ test: /\.woff2$/,   loader: "url-loader?limit=10000&minetype=application/font-woff2" },
-			{ test: /\.woff$/,   loader: "url-loader?limit=10000&minetype=application/font-woff" },
-      { test: /\.ttf$/,    loader: "file-loader" },
-      { test: /\.eot$/,    loader: "file-loader" },
-      { test: /\.svg$/,    loader: "file-loader" },
+			{ test: /\.woff2$/, loader: "url-loader?limit=10000&minetype=application/font-woff2" },
+			{ test: /\.woff$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.ttf$/, loader: "file-loader" },
+      { test: /\.eot$/, loader: "file-loader" },
+      { test: /\.svg$/, loader: "file-loader" },
 			{ test: /\.css$/, loader: 'style-loader!css-loader' },
     ]
   }
